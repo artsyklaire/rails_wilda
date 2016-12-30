a38acac2415a51f37deb4f35508f0f2f8acd39cd
artsyklaire <touclaire@gmail.com>
2016-12-30 00:16:36 -0500

    3.11 updated

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,3 +2,8 @@ class StaticPagesController < ApplicationController
   def index
   end
 end
+class PagesController < ApplicationController
+  def show
+    render template: "static_pages/#{params[:page]}"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
-  get 'static_pages/about'
-
-  get 'static_pages/contact'
-
-  get 'static_pages/index'
-
-  root 'static_pages#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/static_pages/:page" => "pages#show"
 end
+
+
+
+# Rails.application.routes.draw do
+  # get 'static_pages/about'
+
+  # get 'static_pages/contact'
+
+  # get 'static_pages/index'
+
+  # root 'static_pages#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# end
